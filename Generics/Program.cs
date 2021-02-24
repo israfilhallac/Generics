87f4e3bdd233b9b9111234a6afa06ad94cb5ebba
İsrafilHallac <i.hallac1408@gmail.com>
2021-02-24 08:12:03 +0200

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Utilities utilities = new Utilities();
+            List<string> result = utilities.BuildList<string>("Rize", "Artvin", "Trabzon");
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            List<Customer> result2 = utilities.BuildList<Customer>(new Customer { FirstName = "İsrafil" },
+                new Customer { FirstName = "Tomris" });
+            foreach (var customer in result2)
+            {
+                Console.WriteLine(customer.FirstName);
+            }
+            Console.ReadLine();
+        }
+
+    }
+    class Utilities
+    {
+        public List<T> BuildList<T>(params T[] items)
+        {
+            return new List<T>(items);
+        }
+    }
+    class Product : IProductDal
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface IProductDal:IRepository<Product>
+    {
+
+    }
+    class Customer : ICustomerDal
+    {
+        public string FirstName { get; set; }
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    interface ICustomerDal:IRepository<Customer>
+    {
+
+    }
+
+    interface IRepository<T> where T:class,new()
+    {
+        List<T> GetAll();
+        T Get(int Id);
+        void Add();
+        void Delete();
+        void Update();
+    }
+
+}
